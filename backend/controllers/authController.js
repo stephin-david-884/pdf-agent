5d02549f33c616c63ee065d21023dda6c6b56550
Stephin David <stephindavid884@gmail.com>
2026-01-12 00:28:47 +0530

    Auth controller done

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -120,7 +120,19 @@ export const login = async (req, res, next) => {
 //@access Private
 export const getProfile = async (req, res, next) => {
     try {
-        
+        const user = await User.findById(req.user._id);
+
+        res.status(200).json({
+            success: true,
+            data: {
+                id: user._id,
+                username: user.username,
+                email: user.email,
+                profileImage: user.profileImage,
+                createdAt: user.createdAt,
+                updatedAt: user.updatedAt
+            },
+        });
     } catch (error) {
         next(error)
     }
@@ -131,6 +143,26 @@ export const getProfile = async (req, res, next) => {
 //@access Private
 export const updateProfile = async (req, res, next) => {
     try {
+       const { username, email, profileImage } = req.body;
+       
+       const user =  await User.findById(req.user._id);
+
+       if(username) user.username = username;
+       if(email) user.email = email;
+       if(profileImage) user.profileImage = profileImage;
+
+       await user.save();
+
+       res.status(200).json({
+            success: true,
+            data: {
+                id: user._id,
+                username: user.username,
+                email: user.email,
+                profileImage: user.profileImage,
+            },
+            message: "Profile updated succesfully"
+        });
         
     } catch (error) {
         next(error)
@@ -143,6 +175,38 @@ export const updateProfile = async (req, res, next) => {
 export const changePassword = async (req, res, next) => {
     try {
         
+        const {currentPassword, newPassword} = req.body;
+
+        if(!currentPassword || !newPassword){
+            return res.status(400).json({
+                success: false,
+                error: "Please provide current and new password",
+                statusCode: 400,
+            });
+        }
+
+        const user = await User.findById(req.user._id).select("+password");
+
+        //Check current password
+        const isMatch = await user.matchPassword(currentPassword);
+
+        if(!isMatch) {
+            return res.status(401).json({
+                success: false,
+                error: "Current password is incorrect",
+                statusCode: 401,
+            });
+        }
+
+        //Update password
+        user.password = newPassword;
+        await user.save();
+
+        res.status(200).json({
+            success: true,
+            message: "Password changed successfully"
+        })
+
     } catch (error) {
         next(error)
     }
