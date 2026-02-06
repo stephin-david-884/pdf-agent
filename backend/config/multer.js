df5f1b2845a3c9a3fcaaa56e353834b2600b5471
Stephin David <stephindavid884@gmail.com>
2026-02-06 23:19:04 +0530

    AI controller completed

--- backend/config/multer.js
+++ backend/config/multer.js
@@ -3,8 +3,8 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
 
-const _filename = fileURLToPath(import.meta.url);
-const _dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const uploadDir = path.join(__dirname, '../uploads/documents');
 if(!fs.existsSync(uploadDir)){
