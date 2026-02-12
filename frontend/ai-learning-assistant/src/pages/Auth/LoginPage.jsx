21ea3fcb73ce2d7fbb39c2d88d12bb5a598a1e13
Stephin David <stephindavid884@gmail.com>
2026-02-12 23:49:56 +0530

    Authcontex setup

--- frontend/ai-learning-assistant/src/pages/Auth/LoginPage.jsx
+++ frontend/ai-learning-assistant/src/pages/Auth/LoginPage.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext';
+import authService from '../../services/authService.js';
+import { BrainCircuit, Mail, Lock, ArrowRight } from  'lucide-react';
+import toast from 'react-hot-toast';
 
 const LoginPage = () => {
+
+  const [email, setEmail] = useState('topay@yopmail.com');
+  const [password, setPassword] = useState('Test@123');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [focusedField, setFocusedField] = useState(null);
+
+  const navigate = useNavigate();
+
+  const { login } = useAuth();
+
   return (
     <div>
-      JI
+      
     </div>
   )
 }
